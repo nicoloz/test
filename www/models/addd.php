9cd9b594b1b37c1efb7ccb09dfa1cc5e2801e6ab
НИКО <nick.semu@outlook.com>
2015-08-18 17:20:02 +0300

    Создали файл addd.php

--- www/models/addd.php
+++ www/models/addd.php
@@ -1,1 +1,22 @@
 <?php
+if(!empty($_POST['data'])){
+    $data = $_POST['data'];
+}else{header ('Location: ../view/add.php');}
+
+if(!empty($_POST['name'])){
+    $name = $_POST['name'];
+}else{header ('Location: ../view/add.php');}
+
+if(!empty($_POST['text'])){
+    $text = $_POST['text'];
+}else{header ('Location: ../view/add.php');}
+//-------------------------------------------------------------------------
+require __DIR__.'/../functions.php';
+connect('localhost','vaso','1234','news');
+//-------------------------------------------------------------------------
+$query = "INSERT INTO articls (data,name,text) VALUES ('".$data."','".$name."','".$text."')";
+mysql_query($query);
+//-------------------------------------------------------------------------
+header ('Location: ../index.php');
+
+?>
